c2e69f93dc79a9ccb2818bb05f434584e63f6dee
Aleksander Jan Bajkowski <olek2@wp.pl>
2021-12-28 23:44:02 +0100

    net: lantiq_etop: add stats support
    This patch adds support for software packet and byte counters.
    
    Signed-off-by: Aleksander Jan Bajkowski <olek2@wp.pl>

--- drivers/net/ethernet/lantiq_etop.c
+++ drivers/net/ethernet/lantiq_etop.c
@@ -145,6 +145,8 @@ ltq_etop_hw_receive(struct ltq_etop_chan *ch)
 	skb_put(skb, len);
 	skb->protocol = eth_type_trans(skb, ch->netdev);
 	netif_receive_skb(skb);
+	ch->netdev->stats.rx_packets++;
+	ch->netdev->stats.rx_bytes += len;
 }
 
 static int
@@ -182,6 +184,8 @@ ltq_etop_poll_tx(struct napi_struct *napi, int budget)
 	spin_lock_irqsave(&priv->lock, flags);
 	while ((ch->dma.desc_base[ch->tx_free].ctl &
 			(LTQ_DMA_OWN | LTQ_DMA_C)) == LTQ_DMA_C) {
+		ch->netdev->stats.tx_packets++;
+		ch->netdev->stats.tx_bytes += ch->skb[ch->tx_free]->len;
 		dev_kfree_skb_any(ch->skb[ch->tx_free]);
 		ch->skb[ch->tx_free] = NULL;
 		memset(&ch->dma.desc_base[ch->tx_free], 0,
@@ -482,6 +486,7 @@ ltq_etop_tx(struct sk_buff *skb, struct net_device *dev)
 	u32 byte_offset;
 
 	if (skb_put_padto(skb, ETH_ZLEN))
+		dev->stats.tx_dropped++;
 		return NETDEV_TX_OK;
 
 	if ((desc->ctl & (LTQ_DMA_OWN | LTQ_DMA_C)) || ch->skb[ch->dma.desc]) {
