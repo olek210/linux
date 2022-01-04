9723bceb42a3115db375008a0edd5b8601d81d14
Aleksander Jan Bajkowski <olek2@wp.pl>
2022-01-04 01:06:19 +0100

    net: lantiq_xrx200: use skb cache
    NAT performance results on BT Home Hub 5A (kernel 5.15.45):
    
    Fast path:
    	Up	Down
    Before	702.4	719.3
    After	707.3	739.9
    
    Slow path:
    	Up	Down
    Before	91.8	184.1
    After	92.0	185.7
    
    Signed-off-by: Aleksander Jan Bajkowski <olek2@wp.pl>

--- drivers/net/ethernet/lantiq_xrx200.c
+++ drivers/net/ethernet/lantiq_xrx200.c
@@ -238,7 +238,7 @@ static int xrx200_hw_receive(struct xrx200_chan *ch)
 		return ret;
 	}
 
-	skb = build_skb(buf, priv->rx_skb_size);
+	skb = napi_build_skb(buf, priv->rx_skb_size);
 	skb_reserve(skb, NET_SKB_PAD);
 	skb_put(skb, len);
 
@@ -321,7 +321,7 @@ static int xrx200_tx_housekeeping(struct napi_struct *napi, int budget)
 			pkts++;
 			bytes += skb->len;
 			ch->skb[ch->tx_free] = NULL;
-			consume_skb(skb);
+			napi_consume_skb(skb, budget);
 			memset(&ch->dma.desc_base[ch->tx_free], 0,
 			       sizeof(struct ltq_dma_desc));
 			ch->tx_free++;
